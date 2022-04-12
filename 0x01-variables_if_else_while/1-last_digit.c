9c9e4edc9d63c6c35847a931e93d1041f60f9b71
Bereket <berkeyman468@gmail.com>
2022-04-12 14:13:35 +0300

    If else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
 int n, lastd;
